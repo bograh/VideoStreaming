26c1761709708950f098b7d6e281caeafbd32e75
Bernard Ograh <ograhbernard@gmail.com>
2026-03-17 20:20:30 +0000

    feat: Add AsyncConfig for managing asynchronous task execution
    - Introduced `AsyncConfig` with a thread pool executor to support asynchronous operations.
    - Configured core pool size, max pool size, queue capacity, and custom thread name prefix.

---
+++ src/main/java/dev/ograh/videostreaming/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package dev.ograh.videostreaming.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "asyncExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsynchThread-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+}
