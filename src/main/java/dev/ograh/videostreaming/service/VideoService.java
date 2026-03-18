c325fedf881f0a2d4d74cdfd1206a4f89d99a710
Bernard Ograh <ograhbernard@gmail.com>
2026-03-18 14:57:42 +0000

    feat: Add caching and pagination support in `VideoService`
    - Implemented `@Cacheable` and `@CacheEvict` annotations for efficient caching of video data.
    - Added methods for retrieving videos with pagination and dynamic sorting.
    - Introduced `getVideos` and `getVideo` to fetch paginated video summaries and detailed video responses, leveraging projections.
    - Enhanced `validateVideoFile` to include title-based duplication checks.

--- src/main/java/dev/ograh/videostreaming/service/VideoService.java
+++ src/main/java/dev/ograh/videostreaming/service/VideoService.java
@@ -1,24 +1,40 @@
 package dev.ograh.videostreaming.service;
 
+import dev.ograh.videostreaming.dto.projection.VideoProjection;
+import dev.ograh.videostreaming.dto.projection.VideoSummaryProjection;
+import dev.ograh.videostreaming.dto.projection.VideoTagProjection;
 import dev.ograh.videostreaming.dto.request.VideoUploadRequest;
+import dev.ograh.videostreaming.dto.response.VideoResponse;
+import dev.ograh.videostreaming.dto.response.VideoSummaryResponse;
 import dev.ograh.videostreaming.dto.response.VideoUploadResponse;
+import dev.ograh.videostreaming.dto.shared.PageResponse;
 import dev.ograh.videostreaming.dto.shared.UploadResult;
 import dev.ograh.videostreaming.dto.shared.VideoMetadata;
 import dev.ograh.videostreaming.entity.User;
 import dev.ograh.videostreaming.entity.Video;
 import dev.ograh.videostreaming.entity.VideoFile;
+import dev.ograh.videostreaming.exception.ResourceNotFoundException;
 import dev.ograh.videostreaming.exception.VideoUploadException;
+import dev.ograh.videostreaming.repository.VideoFileRepository;
 import dev.ograh.videostreaming.repository.VideoRepository;
 import dev.ograh.videostreaming.utils.UserHelper;
 import dev.ograh.videostreaming.utils.VideoMetadataUtil;
 import dev.ograh.videostreaming.utils.VideoServiceHelper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,9 +44,11 @@ public class VideoService {
     private final UserHelper userHelper;
     private final VideoMetadataUtil videoMetadataUtil;
     private final VideoServiceHelper videoServiceHelper;
+    private final VideoFileRepository videoFileRepository;
 
+    @CacheEvict(value = "videos", allEntries = true)
     public VideoUploadResponse uploadVideo(MultipartFile file, MultipartFile thumbnail, VideoUploadRequest videoUploadRequest, HttpServletRequest request) {
-        videoServiceHelper.validateVideoFile(file);
+        videoServiceHelper.validateVideoFile(file, videoUploadRequest.title());
         User user = userHelper.getAuthenticatedUser(request);
 
         File tempVideoFile = null;
@@ -65,4 +83,49 @@ public class VideoService {
             videoServiceHelper.deleteTempFile(tempThumbnailFile);
         }
     }
+
+    @Cacheable(
+            value = "videos",
+            condition = "#search == null",
+            key = "'page=' + #page + ',size=' + #size + ',sortBy=' + #sortBy + ',orderBy=' + #orderBy"
+    )
+    public PageResponse<List<VideoSummaryResponse>> getVideos(int page, int size, String sortBy, String orderBy, String search) {
+        Pageable pageable = videoServiceHelper.createPageable(page, Math.min(size, 50), sortBy, orderBy);
+
+        Page<VideoSummaryProjection> videoPage = videoRepository.findAllVideoSummaries(pageable);
+        List<UUID> videoIds = videoPage.stream().map(VideoSummaryProjection::getId).toList();
+
+        List<VideoTagProjection> tags = videoRepository.findTagsByVideoIds(videoIds);
+
+        Map<UUID, List<String>> tagsMap = tags.stream().collect(
+                Collectors.groupingBy(VideoTagProjection::getVideoId,
+                        Collectors.mapping(VideoTagProjection::getTag, Collectors.toList())));
+
+        List<VideoSummaryResponse> videos = videoPage.stream()
+                .map(v -> videoServiceHelper.buildVideoSummaryResponse(v, tagsMap))
+                .toList();
+
+        return new PageResponse<>(
+                videos,
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                videoPage.getTotalPages(),
+                videoPage.getTotalElements(),
+                videoPage.isFirst(),
+                videoPage.isLast(),
+                videoPage.getNumberOfElements(),
+                videoPage.getSort().toString()
+        );
+    }
+
+    @Cacheable(value = "videos", key = "#videoId")
+    public VideoResponse getVideo(String videoId) {
+        UUID videoUuid = UUID.fromString(videoId);
+        VideoProjection video = videoRepository.findProjectedVideo(videoUuid).orElseThrow(
+                () -> new ResourceNotFoundException("Video not found with id: " + videoId)
+        );
+
+        return videoServiceHelper.buildVideoResponseFrom(video);
+    }
+
 }
