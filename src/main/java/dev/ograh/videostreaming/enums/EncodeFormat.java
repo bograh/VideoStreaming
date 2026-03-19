0abded74cd9b003166048488d8e8c9287789ee5b
Bernard Ograh <ograhbernard@gmail.com>
2026-03-19 10:00:53 +0000

    chore: Remove `H265` support from `EncodeFormat` enum

--- src/main/java/dev/ograh/videostreaming/enums/EncodeFormat.java
+++ src/main/java/dev/ograh/videostreaming/enums/EncodeFormat.java
@@ -1,12 +1,11 @@
 package dev.ograh.videostreaming.enums;
 
 public enum EncodeFormat {
-    ORIGINAL, H264, H265, AV1, MPEG4;
+    ORIGINAL, H264, AV1, MPEG4;
 
     public String getFfmpegCodec() {
         return switch (this) {
             case H264 -> "libx264";
-            case H265 -> "libx265";
             case AV1 -> "libaom-av1";
             case MPEG4 -> "mpeg4";
             case ORIGINAL -> null;
@@ -16,7 +15,6 @@ public enum EncodeFormat {
     public int getPriority() {
         return switch (this) {
             case H264 -> 1;
-            case H265 -> 2;
             case AV1 -> 3;
             case MPEG4 -> 4;
             case ORIGINAL -> 0;
