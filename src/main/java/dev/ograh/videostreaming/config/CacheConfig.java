b6e6f24951b37ad0cd57fd696621fa2b5b26064a
Bernard Ograh <ograhbernard@gmail.com>
2026-03-24 10:51:35 +0000

    feat: Add `comments` cache with custom configuration in `CacheConfig`

--- src/main/java/dev/ograh/videostreaming/config/CacheConfig.java
+++ src/main/java/dev/ograh/videostreaming/config/CacheConfig.java
@@ -14,22 +14,32 @@ public class CacheConfig {
     @Bean
     public CaffeineCacheManager caffeineCacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCacheNames(List.of("videos", "users"));
+        cacheManager.setCacheNames(List.of("videos", "users", "comments"));
 
         cacheManager.setCaffeine(Caffeine.newBuilder()
                 .expireAfterWrite(Duration.ofMinutes(15))
                 .maximumSize(1000)
+                .recordStats()
+        );
+
+        cacheManager.registerCustomCache("comments", Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofMinutes(15))
+                .maximumSize(500)
+                .recordStats()
+                .build()
         );
 
         cacheManager.registerCustomCache("videos", Caffeine.newBuilder()
                 .expireAfterWrite(Duration.ofMinutes(30))
                 .maximumSize(500)
+                .recordStats()
                 .build()
         );
 
         cacheManager.registerCustomCache("users", Caffeine.newBuilder()
                 .expireAfterWrite(Duration.ofMinutes(60))
                 .maximumSize(1000)
+                .recordStats()
                 .build()
         );
 
